ec054e6fcf0278248dc3cc4c3d0ff15840fa5a1e
Muhammed Javad <muhammedjavad@Muhammeds-MacBook-Air.local>
2024-06-21 21:18:49 +0530

    Merged

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
+  darkMode: 'class',
   content: ["./src/**/*.{html,js,jsx}"],
   // darkMode: ['selector', '[data-mode="dark"]'],
   theme: {
