8749562447d6aac4c7a8b9daf3057fdb5598fcba
Merin Mathew <merinphilaminmathew19@gmail.com>
2024-06-21 20:53:17 +0530

    enabling dark-mode

--- frontend/src/components/partials/admin/Header/Header.jsx
+++ frontend/src/components/partials/admin/Header/Header.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import Container from '../../../utils/Container'
 
 function Header() {
   return (
-    <div>
-      <header className="bg-zinc-50 text-center dark:bg-neutral-700 lg:text-left">
-      <div className="bg-black/5 p-4 text-center text-surface dark:text-white">
-        fsdf
+    <header class="bg-white">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <a href="#" class="-m-1.5 p-1.5">
+          <span class="sr-only">Your Company</span>
+          <img class="h-8 w-auto" src="/assets/logo (2).png" alt=""/>
+        </a>
       </div>
-    </header>
-    </div>
+    
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+      </div>
+    </nav>
+    
+  </header>
+  
   )
 }
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  content: ["./src/**/*.{html,js,jsx}"],
+  // darkMode: ['selector', '[data-mode="dark"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
