d34deaefdb8b783b9bd0d692a7367dacba47c90b
Muhammed Javad <muhammedjavad@Muhammeds-MacBook-Air.local>
2024-06-21 19:09:32 +0530

    Tailwind css configure attemp

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../../components/partials/admin/Header/Header'
+import Footer from '../../components/partials/admin/Footer/Footer'
 
 function Dashboard() {
   return (
     <div>
-      <Header/>
-      
-      <Footer/>
+      <Header />
+
+      <Footer />
     </div>
   )
 }
---
+++ frontend/tailwindcss-config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    { tailwindcss: { config: './tailwindcss-config.js' } }
+  ],
+}
+
