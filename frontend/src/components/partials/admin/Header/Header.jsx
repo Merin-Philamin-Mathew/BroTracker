cf7341a656857ebc769e514f9756cad35aab2408
Merin Mathew <merinphilaminmathew19@gmail.com>
2024-06-21 21:16:22 +0530

    header completed

--- frontend/src/components/partials/admin/Header/Header.jsx
+++ frontend/src/components/partials/admin/Header/Header.jsx
@@ -3,8 +3,8 @@ import Container from '../../../utils/Container'
 
 function Header() {
   return (
-    <header class="bg-white">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <header class="bg-zinc-50 text-center dark:bg-neutral-900">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 " aria-label="Global">
       <div class="flex lg:flex-1">
         <a href="#" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
@@ -13,7 +13,7 @@ function Header() {
       </div>
     
       <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+        <a href="#" class="text-sm font-semibold leading-6 text-zinc-50">Log in <span aria-hidden="true">&rarr;</span></a>
       </div>
     </nav>
     
