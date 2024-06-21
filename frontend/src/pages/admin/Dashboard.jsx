64426ad65db9f8c1053beb734522e40e683ab0a1
Merin Mathew <merinphilaminmathew19@gmail.com>
2024-06-21 17:36:28 +0530

    basic structure

---
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/partials/admin/Header/Header'
+
+function Dashboard() {
+  return (
+    <div>
+      <Header/>
+      
+      <Footer/>
+    </div>
+  )
+}
+
+export default Dashboard
