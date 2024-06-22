2b26ac42fe2fa74a5770d327b5a7fc4b581307d0
Muhammed Javad <muhammedjavad@Muhammeds-MacBook-Air.local>
2024-06-22 09:44:49 +0530

    before merge

--- frontend/src/components/partials/admin/Header/Header.jsx
+++ frontend/src/components/partials/admin/Header/Header.jsx
@@ -4,21 +4,21 @@ import Container from '../../../utils/Container'
 function Header() {
   return (
     <header class="bg-zinc-50 text-center dark:bg-neutral-900">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 " aria-label="Global">
-      <div class="flex lg:flex-1">
-        <a href="#" class="-m-1.5 p-1.5">
-          <span class="sr-only">Your Company</span>
-          <img class="h-8 w-auto" src="/assets/logo (2).png" alt=""/>
-        </a>
-      </div>
-    
-      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="#" class="text-sm font-semibold leading-6 text-zinc-50">Log in <span aria-hidden="true">&rarr;</span></a>
-      </div>
-    </nav>
-    
-  </header>
-  
+      <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 " aria-label="Global">
+        <div class="flex lg:flex-1">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Your Company</span>
+            <img class="h-8 w-auto" src="/assets/logo (2).png" alt="" />
+          </a>
+        </div>
+
+        <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" class="text-sm font-semibold leading-6 text-zinc-50">Log in <span aria-hidden="true">&rarr;</span></a>
+        </div>
+      </nav>
+
+    </header>
+
   )
 }
 
--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from '../../components/partials/admin/Header/Header'
 import Footer from '../../components/partials/admin/Footer/Footer'
+import Table from '../../components/table/Table'
 import Container from '../../components/utils/Container'
+// import Container from '../../components/utils/Container'
 
 function Dashboard() {
   return (
     <>
       <Header />
+      <Container>
+        <Table />
 
+      </Container>
       <Footer />
     </>
   )
